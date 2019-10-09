57f6087fce22055416bbd748986e6ed5a25bda34
Marcin Kowalski <m.kowalski.nov7@outlook.com>
2019-10-09 11:43:11 +0200

    Update int.a

--- Ubuntu/evalsys.sh
+++ Ubuntu/evalsys.sh
@@ -6,7 +6,7 @@
 
 #--------------------------------------------------------------------------------
 # changelog:
-# v1.0.2 build 0910.19 - fix parseFile, add grep -w (whole words)
+# v1.0.2 build 0910.19 - fix parseFile, add grep -w (whole words), add subsubvalue
 # v1.0.1 build 0810.19 - fix checkInt, add subinterfaces support
 #
 #--------------------------------------------------------------------------------
@@ -60,11 +60,11 @@ function checkUser
 }
 
 #---------------------------------- parseFile ------------------------------------
-# usage: parseFile <file> <param> <value> <subvalue>
+# usage: parseFile <file> <param> <value> <subvalue> <subsub>
 
 function parseFile
 {
-	if [ $(cat "$1" | grep -vE '^(\s*$|#)' | grep -w "$2" | grep -w "$3" | grep -w "$4" -c) -gt 0 ]; then
+	if [ $(cat "$1" | grep -vE '^(\s*$|#)' | grep -w "$2" | grep -w "$3" | grep -w "$4" | grep -w "$5" -c) -gt 0 ]; then
 		echo 1
 	else
 		echo 0
